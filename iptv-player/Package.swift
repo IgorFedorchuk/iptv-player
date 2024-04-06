1469b79c49f95d42423c0fcd08a6e11fd2f011b9
Igor <>
2024-04-06 21:47:47 +0200

    platforms: [.iOS(.v12)],

--- iptv-player/Package.swift
+++ iptv-player/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "iptv-player",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
