d3b0410d53dc08f106f9d7acfaac7b076d42e1e3
Igor <>
2024-04-07 10:47:56 +0200

    add stub project

---
+++ Example/player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  player
+//
+//  Created by Igor Fedorchuk on 07.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
