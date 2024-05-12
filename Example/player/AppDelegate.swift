9639946dc818ca76d14a6aa275afef1febba24ec
Igor <>
2024-05-12 14:51:36 +0200

    INFOPLIST_KEY_UIRequiresFullScreen = YES;

--- Example/player/AppDelegate.swift
+++ Example/player/AppDelegate.swift
@@ -29,6 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.all
+    }
+    
     private func setupAudioSession() {
         let audioSession = AVAudioSession.sharedInstance()
         try? audioSession.setCategory(.playback, mode: .moviePlayback, options: [])
